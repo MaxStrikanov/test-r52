7fe38c67e11062b52259798dd592020b3225370a
Maksim Strikanov <maxvell.88@mail.ru>
2023-11-09 22:50:17 +0300

    Menu

--- components/burgerMenu.js
+++ components/burgerMenu.js
@@ -8,11 +8,12 @@ export const burgerMenu = () => {
 		if(!document.querySelector('.burger__checkbox').checked) {
 			menu.style.transform = 'translateX(0%)'
 			content.style.background = 'rgba(0, 0, 0, 0.6)'
+			content.style.width= 'calc(100% - 300px)'
 		} else {
 			menu.style.transform = 'translateX(-400%)'
 			content.style.background = 'transparent'
+			content.style.width= '100%'
 		}
 		
-})
-
+	})
 }
